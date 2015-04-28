e9859c53e6393dc28539d28c0249d9063a4be8ec
Mike Nash <michael.nash@weightwatchers.com>
2015-04-27 20:17:36 -0700

    Consolidate settings into a single build file

--- project/Build.scala
+++ project/Build.scala
@@ -1,11 +1,14 @@
 import sbt._
 import Keys._
+import wartremover._
+import com.typesafe.sbt.SbtGit._
 
 object QualityBuild extends Build {
 
 	val appName = "quality"
 	val ScalaVersion = "2.11.6"
 	val javacVersion = "1.8"
+	sbtVersion := "0.13.7"
 
 	val commonSettings = Project.defaultSettings ++ 
 		Seq(
@@ -17,14 +20,18 @@ object QualityBuild extends Build {
 				"-feature")
 		)
 
-	lazy val main = Project(
-		id = "main",
-		base = file("."),
-		settings = commonSettings ++ Seq(
+        wartremoverErrors ++= Warts.unsafe
+
+
+	lazy val main = (project in file("."))
+ 		.enablePlugins(com.typesafe.sbt.GitBranchPrompt)
+		.settings(commonSettings: _*)
+		.settings(
 			libraryDependencies ++= Seq(
 				"org.scalatest" % "scalatest_2.11" % "2.2.4" % "test",
-        "org.scalactic" % "scalactic_2.11" % "2.2.4"
-			)
+        			"org.scalactic" % "scalactic_2.11" % "2.2.4"
+			),
+			version := "1.0"
 		)
-	)
+	
 }
